4d6c427e6b095e291387db0c4f7e047fa89d5b91
AngelRamos27 <l20121028@morelia.tecnm.mx>
2024-01-09 01:55:48 -0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Tecnologies from './Routes/Tecnologies'
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: '/portafolio/',
       element: <Base />,
       children: [
         { path: '/aboutme', element: <AboutMe /> },
