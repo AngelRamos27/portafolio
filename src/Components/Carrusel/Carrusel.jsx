be4ee7dfdc2e13f516567fc9c1fcadb064e907b3
AngelRamos27 <l20121028@morelia.tecnm.mx>
2024-01-09 18:43:45 -0600

    acabé

--- src/Components/Carrusel/Carrusel.jsx
+++ src/Components/Carrusel/Carrusel.jsx
@@ -27,10 +27,10 @@ const Carrusel = ({ slides }) => {
     return (
         <div className="h-72 w-full  overflow-hidden">
             {slides.map((slide, index) => (
-                <div key={index} className={`absolute h-52 w-72 lg:w-[40rem] sm:w-[30rem]  rounded-lg transition-opacity duration-500 ${index === currentSlide ? 'opacity-100' : 'opacity-0'}`}>
+                <div key={index} className={`absolute h-52 w-80 lg:w-[45rem] sm:w-[30rem]  rounded-lg transition-opacity duration-500 ${index === currentSlide ? 'opacity-100' : 'opacity-0'}`}>
                     <div className="duration-700 ease-in-out">
                         <>
-                            <img src={slide} className="w-full border-b-2 border-b-amarilloMZ rounded-t-lg h-72 lg:h-[28rem]" alt={`Slide ${index}`} />
+                            <img src={slide} className="w-full border-b-2  object-center border-b-amarilloMZ rounded-t-lg h-72 lg:h-[28rem]" alt={`Slide ${index}`} />
                         </>
                     </div>
                 </div>
